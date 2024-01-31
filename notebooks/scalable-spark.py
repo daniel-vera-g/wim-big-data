6f622f26197214f4110058c96650b2473bdb33f6
danielVg <danielveragi@gmail.com>
2024-01-31 11:48:11 +0100

    WIP optimized spark

--- notebooks/scalable-spark.py
+++ notebooks/scalable-spark.py
@@ -13,7 +13,7 @@ findspark.init()
 jarConfigPath = "/Users/d.veragillard/edu/semester/WIM-1/big-data-advanced-database/bd-project/postgresql-42.7.1.jar"
 
 # Spark configuration
-allocated_memory = "5g"  
+allocated_memory = "6g"
 allocated_cores = "6"  
 
 # Database configuration
@@ -55,9 +55,10 @@ def collect_data(spark, database_url, properties, limit, offset):
     
     artist_query = f"(SELECT * FROM artist order by name LIMIT {limit} OFFSET {offset}) AS temp_table"
     area_query = f"(SELECT * FROM area order by name LIMIT {limit} OFFSET {offset}) AS temp_table"
-    # language_query = f"(SELECT * FROM language LIMIT {limit} OFFSET {offset}) AS temp_table"
-    # alias_query = f"(SELECT * FROM artist_alias LIMIT {limit} OFFSET {offset}) AS temp_table"
+    language_query = f"(SELECT * FROM language LIMIT {limit} OFFSET {offset}) AS temp_table"
+    alias_query = f"(SELECT * FROM artist_alias LIMIT {limit} OFFSET {offset}) AS temp_table"
 
+    # TODO DOING!
     
     # Read data from artist and area tables with only necessary columns
     artist_df = spark.read.jdbc(url=database_url, table=artist_query, properties=properties).select("id", "name", "area")
@@ -71,20 +72,20 @@ def collect_data(spark, database_url, properties, limit, offset):
     artist_country_df = artist_country_df.select(artist_df.name, area_df.name.alias("country"))
     
     # Read more that could be useful for the analysis
-    # language_df = spark.read.jdbc(url=database_url, table="language", properties=properties).select("id", "name")
-    # alias_df = spark.read.jdbc(url=database_url, table="artist_alias", properties=properties).select("artist", "name")
+    language_df = spark.read.jdbc(url=database_url, table=language_query, properties=properties).select("id", "name")
+    alias_df = spark.read.jdbc(url=database_url, table=alias_query, properties=properties).select("artist", "name")
     
     # Join tables...
     # Use explicit column names to avoid ambiguity
-    # artist_language_df = artist_df.join(language_df, artist_df.id == language_df.id).select(artist_df.name, language_df.name.alias("language"))
-    # artist_alias_df = artist_df.join(alias_df, artist_df.id == alias_df.artist).select(artist_df.name, alias_df.name.alias("alias"))
+    artist_language_df = artist_df.join(language_df, artist_df.id == language_df.id).select(artist_df.name, language_df.name.alias("language"))
+    artist_alias_df = artist_df.join(alias_df, artist_df.id == alias_df.artist).select(artist_df.name, alias_df.name.alias("alias"))
     
     # Combining all data into one dataframe with left outer join
-    # combined_artist_df = artist_country_df \
-    #     .join(artist_alias_df, ["name"], "left_outer") \
-    #     .join(artist_language_df, ["name"], "left_outer")
+    combined_artist_df = artist_country_df \
+        .join(artist_alias_df, ["name"], "left_outer") \
+        .join(artist_language_df, ["name"], "left_outer")
 
-    combined_artist_df = artist_country_df
+    logger(combined_artist_df.show(10))
 
     return combined_artist_df
 
@@ -100,6 +101,8 @@ def clean_data(combined_artist_df):
     # Remove all rows in combined_artist_df that have null values
     combined_artist_df = combined_artist_df.na.drop()
 
+    logger(f"Number of rows after dropping null values: {combined_artist_df.count()}")
+
     return combined_artist_df
 
 from pyspark.ml.feature import StringIndexer, OneHotEncoder, VectorAssembler, StandardScaler
@@ -111,15 +114,16 @@ def transform_data(combined_artist_df):
     # 2. Then use `OneHotEncoder` to convert the categorical indices into a binary vector(F.ex `[0,1,0,...]`)
 
     # String Indexing for all categorical columns
-    indexers = [StringIndexer(inputCol=column, outputCol=column+"_index").fit(combined_artist_df) 
-                for column in ["name", "country"]]
-                # for column in ["name", "country", "language", "alias"]]
+    indexers = [StringIndexer(inputCol=column, outputCol=column+"_index", handleInvalid="keep").fit(combined_artist_df) 
+            for column in ["name"]]  # Exclude 'country'
     
+    label_stringIdx = StringIndexer(inputCol="country", outputCol="country_index")
+
     # One-Hot Encoding for all indexed columns
     encoders = [OneHotEncoder(inputCol=indexer.getOutputCol(), outputCol=indexer.getOutputCol()+"_vec")
                 for indexer in indexers]
 
-    return indexers, encoders
+    return indexers, encoders, label_stringIdx
 
 def normalize_data(encoders):
     # Scale transformed values to fixed range. To do this:
@@ -135,11 +139,11 @@ def normalize_data(encoders):
 
     return assembler , scaler
 
-def run_preprocessing_pipeline(combined_artist_df, indexers, encoders, assembler, scaler):
+def run_preprocessing_pipeline(combined_artist_df, indexers, encoders, label_stringIdx, assembler, scaler):
     # Finally combine all steps into one transformation / normalization pipeline and run it:
 
     # Building a Pipeline for transformations
-    pipeline = Pipeline(stages=indexers + encoders + [assembler, scaler])
+    pipeline = Pipeline(stages=indexers + encoders + [assembler, scaler, label_stringIdx])
 
     
     # Transforming the data
@@ -169,15 +173,22 @@ from pyspark.ml.classification import LogisticRegression
 
 def train_model(train_data):
     # Initialize the Logistic Regression model
+    from pyspark.ml.linalg import Vectors
+
+    # Convert the scaledFeatures column to DenseVector
+    from pyspark.ml.linalg import VectorUDT
+
+    train_data = train_data.withColumn("scaledFeatures", train_data["scaledFeatures"].cast(VectorUDT()))
+
     lr = LogisticRegression(featuresCol="scaledFeatures", labelCol="country_index")
-    
+
     # Fit the model on the training data
     lrModel = lr.fit(train_data)
-    
+
     # logger the coefficients and intercept
     logger("Coefficients: " + str(lrModel.coefficientMatrix))
     logger("Intercept: " + str(lrModel.interceptVector))
-    
+
     # You can also logger a summary of the model over the training set
     trainingSummary = lrModel.summary
     logger("Accuracy: ", trainingSummary.accuracy)
@@ -242,10 +253,14 @@ def test_model(cvModel, evaluator, test_data):
 # Function to process and write each batch
 def process_and_write_batch(batch_df, batch_id, output_dir):
     # Apply transformations and cleaning
+    # print batch
     batch_df = clean_data(batch_df)
+    if batch_df.count() == 0:
+        logger(f"Batch {batch_id} is empty. Skipping ...")
+        return 0
     transformation_data = transform_data(batch_df)
     normalized_data = normalize_data(transformation_data[1])
-    processed_data = run_preprocessing_pipeline(batch_df, transformation_data[0], transformation_data[1], normalized_data[0], normalized_data[1])
+    processed_data = run_preprocessing_pipeline(batch_df, transformation_data[0], transformation_data[1], transformation_data[2], normalized_data[0], normalized_data[1])
 
     # Write processed batch to disk in Parquet format
     batch_output_path = f"{output_dir}/batch_{batch_id}.parquet"
@@ -253,12 +268,14 @@ def process_and_write_batch(batch_df, batch_id, output_dir):
     logger(f"Writing batch {batch_id} to {batch_output_path}")
     processed_data.write.mode('overwrite').parquet(batch_output_path)
 
+    return 1
+
 ## --- Main --- ##
 
 # Incrementally process query the database and process each batch for each feature
-# for feature in features:
-    # Check how much rows are in feature table
 total_rows = spark.read.jdbc(url=database_url, table="artist", properties=properties).count()
+num_batches = total_rows // fetchsize
+num_batches = 0
 
 logger(f"Total number of rows in feature table: {total_rows}")
 
@@ -270,21 +287,23 @@ for offset in range(0, total_rows, fetchsize):
     combined_artist_df = collect_data(spark, database_url, properties, fetchsize, offset)
 
     if combined_artist_df.count() == 0:
-        # logger(f"loggerBatch {batch_id} is empty. Skipping ...")
+        logger(f"loggerBatch {batch_id} is empty. Skipping ...")
         continue
 
     logger(f"Number of rows in batch {batch_id}: {combined_artist_df.count()}")
 
-    process_and_write_batch(combined_artist_df, batch_id, output_dir)
+    # Count the number of batches processed to determine the number of batches to combine later
+    isBatchProcessed = process_and_write_batch(combined_artist_df, batch_id, output_dir)
+    num_batches += isBatchProcessed
 
 # Reading and combining processed batches
 combined_df = None
-num_batches = total_rows // fetchsize
+
+logger(f"Number of batches processed: {num_batches}")
 
 for batch_id in range(num_batches):
     batch_output_path = f"{output_dir}/batch_{batch_id}.parquet"
     if not os.path.exists(batch_output_path):
-        # logger(f"[MAIN_COMBINING]: Batch {batch_id} is missing. Skipping ...")
         continue
 
     batch_df = spark.read.parquet(batch_output_path)
@@ -294,7 +313,8 @@ for batch_id in range(num_batches):
     if combined_df is None:
         combined_df = batch_df
     else:
-        combined_df = combined_df.union(batch_df).distinct()
+        # combined_df = combined_df.union(batch_df).distinct()
+        combined_df = combined_df.unionAll(batch_df)
 
 logger(f"Number of rows in combined_df: {combined_df.count()}")
 
